8453c3e32095afc421f2d4c06712715d7f85be9b
Danila Sivodedov <dan-sivod@mail.ru>
2017-05-08 14:38:58 +0300

    first title

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
 <?php
+
+class Article
+{
+    public $title;
+    public $text;
+    public function new()
+    {
+        echo $this->title;
+    }
+
+}
+
+$a = new Article;
+$a->title = 'First title';
+$a->new();
