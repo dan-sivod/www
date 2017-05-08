4f210dc9b660210df62758ac1ee44118403e5919
Danila Sivodedov <dan-sivod@mail.ru>
2017-05-08 14:58:34 +0300

    first construct

--- index.php
+++ index.php
@@ -4,13 +4,19 @@ class Article
 {
     public $title;
     public $text;
+    public function __construct( $title, $text )
+    {
+        $this->title = $title;
+        $this->text = $text;
+    }
     public function new()
     {
         echo $this->title;
+        echo '<br/>';
+        echo $this->text;
     }
 
 }
 
-$a = new Article;
-$a->title = 'First title';
+$a = new Article('First title','first text');
 $a->new();
